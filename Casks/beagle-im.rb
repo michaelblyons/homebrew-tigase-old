adb0214c6326faf9d0a801478e02d694efddda83
Michael Lyons <michael@fireside21.com>
2020-02-21 14:21:56 -0500

    Bump Beagle to v3.6

--- Casks/beagle-im.rb
+++ Casks/beagle-im.rb
@@ -1,6 +1,6 @@
 cask 'beagleim' do
-  version '3.4'
-  sha256 'e00b44abfe5549030c92d4686d10369eb023cf79fae15a132a80606c0fd06583'
+  version '3.6'
+  sha256 'f8ebf241e4eb7953760dc3619393e338943bbe0139ce3b3a8a833592e911a7ef'
 
   # github.com/tigase/beagle-im was verified as official when first introduced to the cask
   url "https://github.com/tigase/beagle-im/releases/download/#{version}/BeagleIM.#{version}.zip"
